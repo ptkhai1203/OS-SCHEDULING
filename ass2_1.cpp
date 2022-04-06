b316517824b8bed2571aaa1d40609c9e93a52a35
ptkhai1203 <ptkhai1203@gmail.com>
2022-04-06 21:06:48 +0700

    add function to read file and relative file

---
+++ ass2_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+class Process{
+public:
+    string name;
+    int arrivalTime;
+    int burst;
+    int priority;
+};
+
+vector<Process> readFile(string filename, int& quanTum){
+    ifstream fi(filename);
+    int nProcess;
+    fi >> nProcess >> quanTum;
+    vector<Process> p(nProcess);
+    for(int i = 0; i < nProcess; ++i){
+        fi >> p[i].name;
+        fi >> p[(p[i].name[p[i].name.length() - 1] - '0') - 1].arrivalTime >> p[(p[i].name[p[i].name.length() - 1] - '0') - 1].burst >> p[(p[i].name[p[i].name.length() - 1] - '0') - 1].priority;
+    }
+    return p;
+}
+
+void FCFS(vector<Process> p){
+    queue<Process> q;
+    sort(p.begin(), p.end(), [&](const Process& a, const Process& b){return a.arrivalTime < b.arrivalTime;});
+    for(int i = 0; i < p.size(); ++i)
+        cout << p[i].name << '\n';
+    for(auto pro : p)
+        q.push(pro);
+
+}
+
+int main(){
+
+    int q;
+    vector<Process> p = readFile("Input.txt", q);
+    
+    cout << q << '\n';
+    
+    FCFS(p);
+
+    return 0;
+}
